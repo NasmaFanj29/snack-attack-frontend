9cf9522259e61aa37293477faf856af19bd2e7d1
nasma29 <fanjnasma@gmail.com>
2026-04-21 19:51:07 +0300

    update chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -139,6 +139,7 @@ function Chatbot({ menuItems = [], addToCart }) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
+          
           messages: conversationHistory.current
         }),
       });
