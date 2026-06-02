1f0dae6360561a64075f224f35b9b8945da60caa
nasma29 <fanjnasma@gmail.com>
2026-06-02 12:03:14 +0300

    changing home page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initSession } from './utils/sessionReset';
+
+initSession();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
